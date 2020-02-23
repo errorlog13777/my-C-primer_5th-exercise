2d2d8bd83404082755f39736fca22d4735cf4ec8
DF <starbfly10377.com>
2020-02-23 18:37:47 +0800

    add ex13_53_HasPtr.cpp

---
+++ ch13/ex13_53_HasPtr.cpp
@@ -0,0 +1,62 @@
+#include <string>
+
+class HasPtr {
+public:
+    friend void swap(HasPtr&, HasPtr&);
+    HasPtr(const std::string &s = std::string());
+    HasPtr(const HasPtr &hp);
+    HasPtr(HasPtr &&p) noexcept;
+    HasPtr& operator=(HasPtr rhs);
+    //HasPtr& operator=(const HasPtr &rhs);
+    //HasPtr& operator=(HasPtr &&rhs) noexcept;
+    ~HasPtr();
+
+private:
+    std::string *ps;
+    int i;
+};
+
+#include <iostream>
+
+inline void swap(HasPtr &lhs, HasPtr &rhs)
+{
+    using std::swap;
+    swap(lhs.ps, rhs.ps);
+    swap(lhs.i, rhs.i);
+    std::cout << "call swap" << std::endl;
+}
+
+HasPtr::HasPtr(const std::string &s) : ps(new std::string(s)), i(0)
+{
+    std::cout << "call constructor" << std::endl;
+}
+
+HasPtr::HasPtr(const HasPtr &hp) : ps(new std::string(*hp.ps)), i(hp.i)
+{
+    std::cout << "call copy constructor" << std::endl;
+}
+
+HasPtr::HasPtr(HasPtr &&p) noexcept : ps(p.ps), i(p.i)
+{
+    p.ps = 0;
+    std::cout << "call move constructor" << std::endl;
+}
+
+HasPtr& HasPtr::operator=(HasPtr rhs)
+{
+    swap(*this, rhs);
+    return *this;
+}
+
+HasPtr::~HasPtr()
+{
+    std::cout << "call destructor" << std::endl;
+    delete ps;
+}
+
+int main()
+{
+    HasPtr hp1("hello"), hp2("World"), *pH = new HasPtr("World");
+    hp1 = hp2;
+    hp1 = std::move(*pH);
+}
